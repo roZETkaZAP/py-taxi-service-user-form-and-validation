03e8b00947d14e5b799b4b7c4373ec62d7a205f3
Igor <zalugin.iv2@gmail.com>
2026-08-24 16:52:38 +0200

    requested changes added

--- taxi/forms.py
+++ taxi/forms.py
@@ -1,15 +1,21 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model
 
 from taxi.models import Driver, Car
+
+User = get_user_model()
 
 
 class DriverLicenseValidationMixin:
 
     def clean_license_number(self):
-        license_number = self.cleaned_data["license_number"]
-        if not license_number[:3].isalpha() or license_number[:3].upper() != license_number[:3]:
+        license_number = self.cleaned_data.get('license_number')
+        if len(license_number) != 8:
+            raise ValidationError("License number is invalid")
+        if (not license_number[:3].isalpha()
+                or license_number[:3].upper() != license_number[:3]):
             raise ValidationError("License number is invalid")
         if not license_number[3:].isnumeric() or len(license_number[3:]) != 5:
             raise ValidationError("License number is invalid")
@@ -18,19 +24,20 @@ class DriverLicenseValidationMixin:
 
 class DriverCreationForm(DriverLicenseValidationMixin, UserCreationForm):
     class Meta(UserCreationForm.Meta):
-        model = Driver
-        fields = UserCreationForm.Meta.fields + ("first_name", "last_name", "license_number",)
+        model = User
+        fields = (UserCreationForm.Meta.fields
+                  + ("first_name", "last_name", "license_number",))
 
 
 class DriverLicenseUpdateForm(DriverLicenseValidationMixin, forms.ModelForm):
     class Meta:
-        model = Driver
+        model = User
         fields = ("first_name", "last_name", "license_number",)
 
 
 class CarCreationFrom(forms.ModelForm):
     drivers = forms.ModelMultipleChoiceField(
-        queryset=Driver.objects.all(),
+        queryset=User.objects.all(),
         widget=forms.CheckboxSelectMultiple)
 
     class Meta:
